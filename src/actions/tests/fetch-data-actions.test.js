65efde38352f64428c47da973288621f6a85ea53
Catrius Pham <catriuspham@gmail.com>
2020-05-25 00:45:00 +0700

    [f] Add tests for actions

---
+++ src/actions/tests/fetch-data-actions.test.js
@@ -0,0 +1,54 @@
+import { fetchCategories, fetchPost, fetchPosts } from 'actions/fetch-data-actions';
+import { CATEGORIES_API_URL, FETCH_CATEGORIES, FETCH_POST, FETCH_POSTS, POSTS_API_URL } from 'utils/constants';
+
+
+describe('fetchPosts', () => {
+  it('should return FETCH_POSTS action', () => {
+    const params = {
+      category: 'some-category',
+    };
+    expect(fetchPosts(params)).toEqual({
+      type: FETCH_POSTS,
+      payload: {
+        request: {
+          method: 'get',
+          url: POSTS_API_URL,
+          params,
+        },
+      },
+    });
+  });
+});
+
+describe('fetchPost', () => {
+  it('should return FETCH_POST action', () => {
+    const params = {
+      slug: 'some-post',
+    };
+    expect(fetchPost(params)).toEqual({
+      type: FETCH_POST,
+      payload: {
+        request: {
+          method: 'get',
+          url: POSTS_API_URL,
+          params,
+        },
+      },
+    });
+  });
+});
+
+describe('fetchCategories', () => {
+  it('should return FETCH_CATEGORIES action', () => {
+    expect(fetchCategories()).toEqual({
+      type: FETCH_CATEGORIES,
+      payload: {
+        request: {
+          method: 'get',
+          url: CATEGORIES_API_URL,
+          params: {},
+        },
+      },
+    });
+  });
+});
